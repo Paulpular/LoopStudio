4cd8814c204537d76d0ac1b250ce7ba40571cd59
Paulpular <156431389+Paulpular@users.noreply.github.com>
2024-05-24 18:49:21 +0100

    commit

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const hamburger1 = document.querySelector(".image-list")
 const hamburger2 = document.querySelector(".image-X")
 const mobile_menu = document.querySelector(".hamburger-menu")
+const div1 = document.querySelector(".main")
+const div2 = document.querySelector(".mobile-footer")
 
 hamburger1.addEventListener('click', ()=>{
     hamburger1.classList.add("invisible")
     hamburger2.classList.add("visible")
     mobile_menu.classList.toggle("switch")
+    div1.classList.toggle("close")
+    div2.classList.toggle("close")
 })
 
 hamburger2.addEventListener('click', ()=>{
     hamburger1.classList.remove("invisible")
     hamburger2.classList.remove("visible")
     mobile_menu.classList.toggle("switch")
+    div1.classList.toggle("close")
+    div2.classList.toggle("close")
 })
